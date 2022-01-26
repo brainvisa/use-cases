20f58ee2db0aba307e80267f42bf0d9ec84ae7ba
sapetnioc <sapetnioc@users.noreply.github.com>
2022-01-26 12:40:21 +0100

    #1 use status in example

--- bv_use_cases/simplest/__main__.py
+++ bv_use_cases/simplest/__main__.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 from capsul.api import Capsul
 
 # Capsul entry point
@@ -12,7 +14,7 @@ capsul = Capsul()
 # instance (user environment) or from a CapsulEngine instance (execution
 # environment, possibly on a remote computer).
 executable = capsul.executable('bv_use_cases.simplest.list_directory')
-executable.path = '/somewhere'
+executable.path = '/'
 
 # To be able to run an executable, it is necessary to get a CapsulEngine
 # instance. If this instance is remote, it is also necessary to connect to
@@ -25,7 +27,8 @@ with capsul.engine() as capsul_engine:
     execution_id = capsul_engine.start(executable)
     capsul_engine.wait(execution_id)
     final_status = capsul_engine.status(execution_id)
-    capsul_engine.raise_for_status(execution_id)
+    pprint(final_status)
+    capsul_engine.raise_for_status(final_status)
 
 # Here the connection to the CapsulEngine is closed. The execution_id may not be
 # valid for another connection to the same engine. This is engine implementation
